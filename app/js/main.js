5b614c8fbe52fe733ce5bd6970f9e0a9314fd980
roma-bashtevych <roma_bashtevich@ukr.net>
2021-05-20 16:01:57 +0300

    fixed mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,21 @@
 $(function(){
-  $('.slider__wrapper').slick({
+  $('.slider__list').slick({
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true,
-  })
+    // autoplay: true,
+  });
 
-  mixitup('.products__content');
+let t = document.querySelector(".design"),
+  e = document.querySelector(".products"),
+  n = {
+    controls: {
+      scope: "local"
+    }
+  };
+mixitup(t, n), mixitup(e, n)
+
+
+  // const product = mixitup('.products__content');
+  // const design = mixitup('.design__content');
 });
